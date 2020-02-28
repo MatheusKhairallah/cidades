1fe1cc5f26a28af38b2205aadb9e60a3a54aac6f
MatheusKhairallah <playermaster10like@gmail.com>
2020-02-28 20:18:57 -0300

    Adicionamento de programa c#

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace cidades
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Santos");
+             Console.WriteLine("São Vicente");
+              Console.WriteLine("Peruibe");
+               Console.WriteLine("Praia Grande");
+                Console.WriteLine("Mongaguá");
+                 Console.WriteLine("Itanhaém");
+        }
+    }
+}
